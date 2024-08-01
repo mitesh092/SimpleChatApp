052e76a21407e8620d475da77ac228f320e025aa
Ayushzinzuvadiya07 <rameshbhaizinzuvadiya1111@gmail.com>
2024-08-01 10:53:03 +0530

    Login ref

--- frontend/src/AuthenticationComponent/Signup.jsx
+++ frontend/src/AuthenticationComponent/Signup.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { Link,  useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const Signup = () => {

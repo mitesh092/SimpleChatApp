6eb283d9c8126c87c662d0c71c063eb8b979e202
unknown <pratikrathod1900@gmail.com>
2024-08-15 23:25:31 -0700

    first PR

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -6,7 +6,8 @@ import MessageContainer from '../../Components/messages/MessageContainer';
 
 const Home = () => {
   return (
-    <div className='flex sm:h-[550px] sm:w-[900px] md:h-[-500px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+    //  i Will add responsive tailwind css in future
+    <div spe className='flex sm:h-[550px] sm:w-[900px] md:h-[-500px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'> 
       <SideBar/>
       <MessageContainer/>
     </div>
